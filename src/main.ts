c8bca2cda42992561eff741bb8312e849600c196
jasondunken <jasondunken@yahoo.com>
2022-10-25 21:05:56 -0400

    increase request payload size

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { AppModule } from "./app.module";
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
 
+import { urlencoded, json } from "express";
+
 declare const module: any;
 
 async function bootstrap() {
@@ -9,6 +11,8 @@ async function bootstrap() {
     app.enableCors();
     // whitelist only allows dto defined properties through in request body
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+    app.use(json({ limit: "50mb" }));
+    app.use(urlencoded({ limit: "50mb", extended: true }));
     await app.listen(process.env.NEST_PORT);
     console.log(`Application is running on: ${await app.getUrl()}`);
 
